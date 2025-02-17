1e8c9b9175b0cc49db8f6207fe8d5630d2801fff
fabio.srsjr <fabio.srsjr@eduge.ch>
2025-02-17 15:37:21 +0100

    Merge confilt

---
+++ KaosFieldClash/Class/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaosFieldClash.Class
+{
+    public class Question
+    {
+        public string question;
+
+        public string correct;
+        public string[] wrong;
+
+
+    }
+}
