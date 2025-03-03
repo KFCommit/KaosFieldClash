362430142bf1ccb1b7f5c809dbba9a81e49db24c
Bobinetttte <painauchocolad@gmail.com>
2025-03-03 14:53:07 +0100

    Finalisation de la class plateau.cs

--- KaosFieldClash/Class/Plateau.cs
+++ KaosFieldClash/Class/Plateau.cs
@@ -14,32 +14,50 @@ namespace KaosFieldClash.Class
 	{
 		// Variables
 
-		private int nbDe = 1;
+
+		private List<Squares> PlateauList;
+		public int nbJoueur;
 
 		// Constructor
-		public Plateau()
+		public Plateau(int nbJoueur)
 		{
-
+			this.nbJoueur = nbJoueur;
+			PlateauList = new List<Squares>
+			{
+				new StartSquare(this.nbJoueur),
+				new QuestionSquare(2),
+				new QuestionSquare(3),
+				new LadderSquare(4, 12),
+				new QuestionSquare(5),
+				new BaseSquare(6),
+				new BaseSquare(7),
+				new BaseSquare(8),
+				new BaseSquare(9),
+				new BaseSquare(10),
+				new QuestionSquare(11),
+				new BaseSquare(12),
+				new QuestionSquare(13),
+				new BaseSquare(14),
+				new BaseSquare(15),
+				new BaseSquare(16),
+				new QuestionSquare(17),
+				new LadderSquare(18, 24),
+				new QuestionSquare(19),
+				new BaseSquare(20),
+				new SlideSquare(21, 8),
+				new BaseSquare(22),
+				new QuestionSquare(23),
+				new BaseSquare(24),
+				new SlideSquare(25, 16),
+				new BaseSquare(26),
+				new BaseSquare(27),
+				new SquareEnd(28),
+			};
 		}
 
 		// Methods
 
-		public int RollDice()
-		{
-			Random rnd = new Random();
 
-			nbDe = rnd.Next(1, 6+1);
-			
-			return nbDe;
-		}
-
-		public async void ViewDice(int numDe, PictureBox pbDe)
-		{
-			pbDe.Image = Image.FromFile("Img/deAnim.gif");
-			await Task.Delay(2000);
-
-			pbDe.Image = Image.FromFile($"Img/de{numDe}.png");
-		}
 	}
 
 }
