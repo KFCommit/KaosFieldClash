30b79b78a96ad33fa5ff20791b336ddff6d76982
Bobinetttte <painauchocolad@gmail.com>
2025-03-03 15:19:01 +0100

    Finalisation du plateau visuel (Form1)

--- KaosFieldClash/Form1.Designer.cs
+++ KaosFieldClash/Form1.Designer.cs
@@ -30,6 +30,33 @@
         {
 			this.Square0 = new System.Windows.Forms.FlowLayoutPanel();
 			this.pbDe = new System.Windows.Forms.PictureBox();
+			this.Square1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square3 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square4 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square5 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square6 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square7 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square8 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square9 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square10 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square11 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square12 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square13 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square14 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square15 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square16 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square17 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square18 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square19 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square20 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square21 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square22 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square23 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square24 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square25 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square26 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Square27 = new System.Windows.Forms.FlowLayoutPanel();
 			((System.ComponentModel.ISupportInitialize)(this.pbDe)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -53,6 +80,303 @@
 			this.pbDe.TabIndex = 1;
 			this.pbDe.TabStop = false;
 			// 
+			// Square1
+			// 
+			this.Square1.BackColor = System.Drawing.Color.Transparent;
+			this.Square1.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square1.ForeColor = System.Drawing.Color.Transparent;
+			this.Square1.Location = new System.Drawing.Point(181, 426);
+			this.Square1.Name = "Square1";
+			this.Square1.Size = new System.Drawing.Size(93, 80);
+			this.Square1.TabIndex = 3;
+			// 
+			// Square2
+			// 
+			this.Square2.BackColor = System.Drawing.Color.Transparent;
+			this.Square2.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square2.ForeColor = System.Drawing.Color.Transparent;
+			this.Square2.Location = new System.Drawing.Point(280, 426);
+			this.Square2.Name = "Square2";
+			this.Square2.Size = new System.Drawing.Size(89, 80);
+			this.Square2.TabIndex = 4;
+			// 
+			// Square3
+			// 
+			this.Square3.BackColor = System.Drawing.Color.Transparent;
+			this.Square3.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square3.ForeColor = System.Drawing.Color.Transparent;
+			this.Square3.Location = new System.Drawing.Point(375, 426);
+			this.Square3.Name = "Square3";
+			this.Square3.Size = new System.Drawing.Size(89, 80);
+			this.Square3.TabIndex = 5;
+			// 
+			// Square4
+			// 
+			this.Square4.BackColor = System.Drawing.Color.Transparent;
+			this.Square4.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square4.ForeColor = System.Drawing.Color.Transparent;
+			this.Square4.Location = new System.Drawing.Point(470, 426);
+			this.Square4.Name = "Square4";
+			this.Square4.Size = new System.Drawing.Size(89, 80);
+			this.Square4.TabIndex = 6;
+			// 
+			// Square5
+			// 
+			this.Square5.BackColor = System.Drawing.Color.Transparent;
+			this.Square5.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square5.ForeColor = System.Drawing.Color.Transparent;
+			this.Square5.Location = new System.Drawing.Point(565, 426);
+			this.Square5.Name = "Square5";
+			this.Square5.Size = new System.Drawing.Size(89, 80);
+			this.Square5.TabIndex = 7;
+			// 
+			// Square6
+			// 
+			this.Square6.BackColor = System.Drawing.Color.Transparent;
+			this.Square6.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square6.ForeColor = System.Drawing.Color.Transparent;
+			this.Square6.Location = new System.Drawing.Point(667, 426);
+			this.Square6.Name = "Square6";
+			this.Square6.Size = new System.Drawing.Size(89, 80);
+			this.Square6.TabIndex = 8;
+			// 
+			// Square7
+			// 
+			this.Square7.BackColor = System.Drawing.Color.Transparent;
+			this.Square7.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square7.ForeColor = System.Drawing.Color.Transparent;
+			this.Square7.Location = new System.Drawing.Point(762, 322);
+			this.Square7.Name = "Square7";
+			this.Square7.Size = new System.Drawing.Size(89, 184);
+			this.Square7.TabIndex = 9;
+			// 
+			// Square8
+			// 
+			this.Square8.BackColor = System.Drawing.Color.Transparent;
+			this.Square8.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square8.ForeColor = System.Drawing.Color.Transparent;
+			this.Square8.Location = new System.Drawing.Point(667, 322);
+			this.Square8.Name = "Square8";
+			this.Square8.Size = new System.Drawing.Size(89, 80);
+			this.Square8.TabIndex = 9;
+			// 
+			// Square9
+			// 
+			this.Square9.BackColor = System.Drawing.Color.Transparent;
+			this.Square9.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square9.ForeColor = System.Drawing.Color.Transparent;
+			this.Square9.Location = new System.Drawing.Point(572, 322);
+			this.Square9.Name = "Square9";
+			this.Square9.Size = new System.Drawing.Size(89, 80);
+			this.Square9.TabIndex = 10;
+			// 
+			// Square10
+			// 
+			this.Square10.BackColor = System.Drawing.Color.Transparent;
+			this.Square10.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square10.ForeColor = System.Drawing.Color.Transparent;
+			this.Square10.Location = new System.Drawing.Point(470, 322);
+			this.Square10.Name = "Square10";
+			this.Square10.Size = new System.Drawing.Size(89, 80);
+			this.Square10.TabIndex = 11;
+			// 
+			// Square11
+			// 
+			this.Square11.BackColor = System.Drawing.Color.Transparent;
+			this.Square11.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square11.ForeColor = System.Drawing.Color.Transparent;
+			this.Square11.Location = new System.Drawing.Point(375, 322);
+			this.Square11.Name = "Square11";
+			this.Square11.Size = new System.Drawing.Size(89, 80);
+			this.Square11.TabIndex = 12;
+			// 
+			// Square12
+			// 
+			this.Square12.BackColor = System.Drawing.Color.Transparent;
+			this.Square12.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square12.ForeColor = System.Drawing.Color.Transparent;
+			this.Square12.Location = new System.Drawing.Point(280, 322);
+			this.Square12.Name = "Square12";
+			this.Square12.Size = new System.Drawing.Size(89, 80);
+			this.Square12.TabIndex = 13;
+			// 
+			// Square13
+			// 
+			this.Square13.BackColor = System.Drawing.Color.Transparent;
+			this.Square13.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square13.ForeColor = System.Drawing.Color.Transparent;
+			this.Square13.Location = new System.Drawing.Point(185, 322);
+			this.Square13.Name = "Square13";
+			this.Square13.Size = new System.Drawing.Size(89, 80);
+			this.Square13.TabIndex = 14;
+			// 
+			// Square14
+			// 
+			this.Square14.BackColor = System.Drawing.Color.Transparent;
+			this.Square14.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square14.ForeColor = System.Drawing.Color.Transparent;
+			this.Square14.Location = new System.Drawing.Point(86, 219);
+			this.Square14.Name = "Square14";
+			this.Square14.Size = new System.Drawing.Size(89, 183);
+			this.Square14.TabIndex = 15;
+			// 
+			// Square15
+			// 
+			this.Square15.BackColor = System.Drawing.Color.Transparent;
+			this.Square15.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square15.ForeColor = System.Drawing.Color.Transparent;
+			this.Square15.Location = new System.Drawing.Point(185, 219);
+			this.Square15.Name = "Square15";
+			this.Square15.Size = new System.Drawing.Size(89, 80);
+			this.Square15.TabIndex = 15;
+			// 
+			// Square16
+			// 
+			this.Square16.BackColor = System.Drawing.Color.Transparent;
+			this.Square16.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square16.ForeColor = System.Drawing.Color.Transparent;
+			this.Square16.Location = new System.Drawing.Point(280, 219);
+			this.Square16.Name = "Square16";
+			this.Square16.Size = new System.Drawing.Size(89, 80);
+			this.Square16.TabIndex = 16;
+			// 
+			// Square17
+			// 
+			this.Square17.BackColor = System.Drawing.Color.Transparent;
+			this.Square17.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square17.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square17.ForeColor = System.Drawing.Color.Transparent;
+			this.Square17.Location = new System.Drawing.Point(375, 219);
+			this.Square17.Name = "Square17";
+			this.Square17.Size = new System.Drawing.Size(89, 80);
+			this.Square17.TabIndex = 17;
+			// 
+			// Square18
+			// 
+			this.Square18.BackColor = System.Drawing.Color.Transparent;
+			this.Square18.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square18.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square18.ForeColor = System.Drawing.Color.Transparent;
+			this.Square18.Location = new System.Drawing.Point(470, 219);
+			this.Square18.Name = "Square18";
+			this.Square18.Size = new System.Drawing.Size(89, 80);
+			this.Square18.TabIndex = 18;
+			// 
+			// Square19
+			// 
+			this.Square19.BackColor = System.Drawing.Color.Transparent;
+			this.Square19.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square19.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square19.ForeColor = System.Drawing.Color.Transparent;
+			this.Square19.Location = new System.Drawing.Point(572, 219);
+			this.Square19.Name = "Square19";
+			this.Square19.Size = new System.Drawing.Size(89, 80);
+			this.Square19.TabIndex = 19;
+			// 
+			// Square20
+			// 
+			this.Square20.BackColor = System.Drawing.Color.Transparent;
+			this.Square20.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square20.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square20.ForeColor = System.Drawing.Color.Transparent;
+			this.Square20.Location = new System.Drawing.Point(667, 115);
+			this.Square20.Name = "Square20";
+			this.Square20.Size = new System.Drawing.Size(89, 184);
+			this.Square20.TabIndex = 20;
+			// 
+			// Square21
+			// 
+			this.Square21.BackColor = System.Drawing.Color.Transparent;
+			this.Square21.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square21.ForeColor = System.Drawing.Color.Transparent;
+			this.Square21.Location = new System.Drawing.Point(572, 115);
+			this.Square21.Name = "Square21";
+			this.Square21.Size = new System.Drawing.Size(89, 80);
+			this.Square21.TabIndex = 20;
+			// 
+			// Square22
+			// 
+			this.Square22.BackColor = System.Drawing.Color.Transparent;
+			this.Square22.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square22.ForeColor = System.Drawing.Color.Transparent;
+			this.Square22.Location = new System.Drawing.Point(470, 115);
+			this.Square22.Name = "Square22";
+			this.Square22.Size = new System.Drawing.Size(89, 80);
+			this.Square22.TabIndex = 21;
+			// 
+			// Square23
+			// 
+			this.Square23.BackColor = System.Drawing.Color.Transparent;
+			this.Square23.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square23.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square23.ForeColor = System.Drawing.Color.Transparent;
+			this.Square23.Location = new System.Drawing.Point(375, 115);
+			this.Square23.Name = "Square23";
+			this.Square23.Size = new System.Drawing.Size(89, 80);
+			this.Square23.TabIndex = 22;
+			// 
+			// Square24
+			// 
+			this.Square24.BackColor = System.Drawing.Color.Transparent;
+			this.Square24.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square24.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square24.ForeColor = System.Drawing.Color.Transparent;
+			this.Square24.Location = new System.Drawing.Point(280, 12);
+			this.Square24.Name = "Square24";
+			this.Square24.Size = new System.Drawing.Size(89, 183);
+			this.Square24.TabIndex = 23;
+			// 
+			// Square25
+			// 
+			this.Square25.BackColor = System.Drawing.Color.Transparent;
+			this.Square25.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square25.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square25.ForeColor = System.Drawing.Color.Transparent;
+			this.Square25.Location = new System.Drawing.Point(375, 12);
+			this.Square25.Name = "Square25";
+			this.Square25.Size = new System.Drawing.Size(89, 80);
+			this.Square25.TabIndex = 23;
+			// 
+			// Square26
+			// 
+			this.Square26.BackColor = System.Drawing.Color.Transparent;
+			this.Square26.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square26.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square26.ForeColor = System.Drawing.Color.Transparent;
+			this.Square26.Location = new System.Drawing.Point(470, 12);
+			this.Square26.Name = "Square26";
+			this.Square26.Size = new System.Drawing.Size(89, 80);
+			this.Square26.TabIndex = 24;
+			// 
+			// Square27
+			// 
+			this.Square27.BackColor = System.Drawing.Color.Transparent;
+			this.Square27.BackgroundImage = global::KaosFieldClash.Properties.Resources.trans;
+			this.Square27.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.Square27.ForeColor = System.Drawing.Color.Transparent;
+			this.Square27.Location = new System.Drawing.Point(565, 12);
+			this.Square27.Name = "Square27";
+			this.Square27.Size = new System.Drawing.Size(89, 80);
+			this.Square27.TabIndex = 25;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -60,6 +384,33 @@
 			this.BackgroundImage = global::KaosFieldClash.Properties.Resources.plateauKFC;
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.ClientSize = new System.Drawing.Size(936, 518);
+			this.Controls.Add(this.Square27);
+			this.Controls.Add(this.Square26);
+			this.Controls.Add(this.Square25);
+			this.Controls.Add(this.Square24);
+			this.Controls.Add(this.Square23);
+			this.Controls.Add(this.Square22);
+			this.Controls.Add(this.Square21);
+			this.Controls.Add(this.Square20);
+			this.Controls.Add(this.Square19);
+			this.Controls.Add(this.Square18);
+			this.Controls.Add(this.Square17);
+			this.Controls.Add(this.Square16);
+			this.Controls.Add(this.Square15);
+			this.Controls.Add(this.Square14);
+			this.Controls.Add(this.Square13);
+			this.Controls.Add(this.Square12);
+			this.Controls.Add(this.Square11);
+			this.Controls.Add(this.Square10);
+			this.Controls.Add(this.Square9);
+			this.Controls.Add(this.Square8);
+			this.Controls.Add(this.Square7);
+			this.Controls.Add(this.Square6);
+			this.Controls.Add(this.Square5);
+			this.Controls.Add(this.Square4);
+			this.Controls.Add(this.Square3);
+			this.Controls.Add(this.Square2);
+			this.Controls.Add(this.Square1);
 			this.Controls.Add(this.Square0);
 			this.Controls.Add(this.pbDe);
 			this.Name = "Form1";
@@ -72,6 +423,33 @@
 		#endregion
 		private System.Windows.Forms.PictureBox pbDe;
 		private System.Windows.Forms.FlowLayoutPanel Square0;
+		private System.Windows.Forms.FlowLayoutPanel Square1;
+		private System.Windows.Forms.FlowLayoutPanel Square2;
+		private System.Windows.Forms.FlowLayoutPanel Square3;
+		private System.Windows.Forms.FlowLayoutPanel Square4;
+		private System.Windows.Forms.FlowLayoutPanel Square5;
+		private System.Windows.Forms.FlowLayoutPanel Square6;
+		private System.Windows.Forms.FlowLayoutPanel Square7;
+		private System.Windows.Forms.FlowLayoutPanel Square8;
+		private System.Windows.Forms.FlowLayoutPanel Square9;
+		private System.Windows.Forms.FlowLayoutPanel Square10;
+		private System.Windows.Forms.FlowLayoutPanel Square11;
+		private System.Windows.Forms.FlowLayoutPanel Square12;
+		private System.Windows.Forms.FlowLayoutPanel Square13;
+		private System.Windows.Forms.FlowLayoutPanel Square14;
+		private System.Windows.Forms.FlowLayoutPanel Square15;
+		private System.Windows.Forms.FlowLayoutPanel Square16;
+		private System.Windows.Forms.FlowLayoutPanel Square17;
+		private System.Windows.Forms.FlowLayoutPanel Square18;
+		private System.Windows.Forms.FlowLayoutPanel Square19;
+		private System.Windows.Forms.FlowLayoutPanel Square20;
+		private System.Windows.Forms.FlowLayoutPanel Square21;
+		private System.Windows.Forms.FlowLayoutPanel Square22;
+		private System.Windows.Forms.FlowLayoutPanel Square23;
+		private System.Windows.Forms.FlowLayoutPanel Square24;
+		private System.Windows.Forms.FlowLayoutPanel Square25;
+		private System.Windows.Forms.FlowLayoutPanel Square26;
+		private System.Windows.Forms.FlowLayoutPanel Square27;
 	}
 }
 
